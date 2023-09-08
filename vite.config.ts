766bd5fd5a77888ad86d7ef644adb78141142a91
Yakin Nikita <yakin95@gmail.com>
2023-09-08 14:23:06 +0500

    feat: replacing rollup with vite

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite';
+const libraryName = 'layoutGridHelper';
+export default defineConfig({
+	plugins: [dts()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: libraryName,
+			formats: ['es', 'cjs', 'iife'],
+			fileName: (format, entryName) => `${entryName}.${format}.js`
+		},
+		rollupOptions: {
+			output: {
+				preserveModules: false
+			}
+		},
+		minify: 'terser'
+	}
+});
